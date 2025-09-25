8adff7574f632a64a0545c90ffd87a49532be37b
César Guerra <warderer@gmail.com>
2025-09-24 19:56:35 -0600

    ✨ Consumiendo API con Axios

---
+++ 03-fetch-axios/axios.js
@@ -0,0 +1,36 @@
+//1. Obtener las referencias a los elementos del DOM
+const boton = document.getElementById('btn-cargar');
+const contenedorResultado = document.getElementById('resultado');
+
+//2. Agregar un evento al botón para cargar los datos
+boton.addEventListener('click', obtenerDatosConFetch);
+
+function obtenerDatosConFetch() {
+    console.log('Cargando datos con Fetch...')
+    contenedorResultado.innerHTML = '<p>Cargando datos...</p>';
+    const URL = 'https://jsonplaceholder.typicode.com/posts?_limit=10'
+
+    //3. Realizar la petición con Fetch
+    axios.get(URL)
+    .then(response => {
+        mostrarDatos(response.data);
+    })
+    .catch(error => {
+        console.error('Error al obtener los datos:', error.message);
+        contenedorResultado.innerHTML = `<p style="color: red;">Error al cargar los datos: ${error.message}</p>`;
+    })
+}
+
+function mostrarDatos(datos) {
+    contenedorResultado.innerHTML = ''; // Limpiar el contenedor antes de mostrar nuevos datos
+
+    datos.forEach(post => {
+        const postElement = document.createElement('article');
+        postElement.classList.add('post');
+        postElement.innerHTML = `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+        `;
+        contenedorResultado.appendChild(postElement);
+    })
+}
